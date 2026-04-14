eae876f7dd4e832feea27226a87085926c87d488
Kiarie <geofreykiarie04@gmail.com>
2026-04-15 00:38:35 +0300

    data ingestion and transformation

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_details(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -9,10 +10,11 @@ def error_message_details(error, error_detail:sys):
     
     return error_message
 
-class CustomerException(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_details:sys):
         super().__init__(error_message)
         self.error_message = error_message_details(error_message, error_detail=error_details)
 
     def __str__(self):
         return self.error_message
+    
