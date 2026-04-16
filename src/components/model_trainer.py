7ada3b196e2a83173ea814436142cb216ccad877
Kiarie <geofreykiarie04@gmail.com>
2026-04-16 21:18:02 +0300

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -40,9 +40,39 @@ class ModelTrainer:
                 #"CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
-
-            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                            models=models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    'n_neighbors': [1,2,3,4,5,6]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            model_report, best_params = evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                            models=models, params=params)
             
             # to get the best model from score dict
             best_model_score = max(sorted(model_report.values()))
@@ -65,7 +95,7 @@ class ModelTrainer:
             predicted = best_model.predict(X_test)
             r2_square = r2_score(y_test, predicted)
 
-            return best_model, r2_square
+            return best_model, best_params, r2_square
         
         except Exception as e:
             raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import dill
 from src.exception import CustomException
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -20,12 +21,18 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_model(X_train,y_train,X_test,y_test, models):
+def evaluate_model(X_train,y_train,X_test,y_test, models, params):
     try:
         report ={}
-        
+
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
@@ -33,8 +40,10 @@ def evaluate_model(X_train,y_train,X_test,y_test, models):
             train_model_score = r2_score(y_train_pred, y_train)
             test_model_score = r2_score(y_test_pred, y_test)
 
+            best_params = gs.best_params_
+
             report[list(models.keys())[i]] = test_model_score
 
-        return report
+        return report, best_params
     except Exception as e:
         raise CustomException(e, sys)
